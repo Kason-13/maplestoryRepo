1d38f2514d88e8fcc2cc092e52270fbebb2dfa3d
ka-son chau <ka.alex.chau@gmail.com>
2020-03-04 23:28:51 -0500

    commented out bug, to check tomorrow

--- js/indexAudio.js
+++ js/indexAudio.js
@@ -11,3 +11,18 @@ window.addEventListener("load",()=>{
        }
    }) 
 })
+
+/*
+window.addEventListener("load",()=>{
+   document.addEventListener("click",()=>{
+       if(!playing){
+            let audio = new Audio();
+            audio.src = "../audio/msLogin.mp3";
+            audio.volume=0.2;
+            audio.muted = false;
+            audio.play();
+            playing = !playing;
+       }
+   }) 
+})
+*/
