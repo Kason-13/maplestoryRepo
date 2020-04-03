3be47cacb8f9eafdb753d5f527b5d503e53c1054
ka-son chau <ka.alex.chau@gmail.com>
2020-04-02 23:30:09 -0400

    started grid

--- jeu.php
+++ jeu.php
@@ -6,6 +6,7 @@
 ?>
     <script src="js\cssManip\js\jQuery.js"></script>
     <script src="js/jeuJS/gameJS.js"></script>
+    <link rel="stylesheet" href="css/jeu.css">
     <title>Jeu</title>
 </head>
 <body>
@@ -16,11 +17,12 @@
             <div class="hp"></div>
         </div>
     </template>
-    <div id="Board-Container">
-        <div id="Oppenent-Hand"></div>
-        <div id="Oppenent-Field"></div>
-        <div id="Player-Field"></div>
-        <div id="Player-Hand"></div>
+    <div id="BoardContainer">
+        <div id="GameInfo"></div>
+        <div id="OppenentHand"></div>
+        <div id="OppenentField"></div>
+        <div id="PlayerField"></div>
+        <div id="PlayerHand"></div>
     </div>
 </body>
 <?php
