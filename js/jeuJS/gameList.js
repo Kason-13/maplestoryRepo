72122da308f1c5ee120511105477c8388a2d215e
ka-son chau <ka.alex.chau@gmail.com>
2020-04-05 18:43:01 -0400

    added most card images

---
+++ js/jeuJS/gameList.js
@@ -0,0 +1,238 @@
+cardData={
+    1:{
+        name:"cardName",
+        img:"",
+    },
+    2:{
+        name:"Shield Golem",
+        img:"images/MonsterImgDB/2.png",
+    },
+    3:{
+        name:"Surprised Cactus",
+        img:"images/MonsterImgDB/3.png",
+    },
+    4:{
+        name:"Red Ribbon Pig",
+        img:"images/MonsterImgDB/4.jpg",
+    },
+    5:{
+        name:"Green Mushroom",
+        img:"images/MonsterImgDB/5.png",
+    },
+    6:{
+        name:"Red Snail",
+        img:"images/MonsterImgDB/6.png",
+    },
+    7:{
+        name:"Zombie Mushroom",
+        img:"images/MonsterImgDB/7.png",
+    },
+    8:{
+        name:"Green Slime",
+        img:"images/MonsterImgDB/8.jpg",
+    },
+    9:{
+        name:"Typical Bandit",
+        img:"images/MonsterImgDB/9.png",
+    },
+    10:{
+        name:"Gachapon",
+        img:"images/MonsterImgDB/10.jpg",
+    },
+    11:{
+        name:"Helmet Pepe",
+        img:"images/MonsterImgDB/11.jpg",
+    },
+    12:{
+        name:"Harry, The Courageous",
+        img:"images/MonsterImgDB/12.jpg",
+    },
+    13:{
+        name:"Stone Golem",
+        img:"images/MonsterImgDB/13.jpg",
+    },
+    14:{
+        name:"Priest",
+        img:"images/MonsterImgDB/14.png",
+    },
+    15:{
+        name:"The Jester",
+        img:"images/MonsterImgDB/15.jpg",
+    },
+    16:{
+        name:"Mihile",
+        img:"images/MonsterImgDB/16.jpg",
+    },
+    17:{
+        name:"The foolish mage",
+        img:"images/MonsterImgDB/17.png",
+    },
+    18:{
+        name:"Aran",
+        img:"images/MonsterImgDB/18.png",
+    },
+    19:{
+        name:"Untamed Black Wyvern",
+        img:"images/MonsterImgDB/19.jpg",
+    },
+    20:{
+        name:"Becoming Dark Lord",
+        img:"images/MonsterImgDB/20.jpg",
+    },
+    21:{
+        name:"Resistance: The Mechanic",
+        img:"images/MonsterImgDB/21.jpg",
+    },
+    22:{
+        name:"Resistance: The Battlemage",
+        img:"images/MonsterImgDB/22.jpg",
+    },
+    23:{
+        name:"Kyrin",
+        img:"images/MonsterImgDB/23.jpg",
+    },
+    24:{
+        name:"cardName",
+        img:"",
+    },
+    25:{
+        name:"cardName",
+        img:"",
+    },
+    26:{
+        name:"cardName",
+        img:"",
+    },
+    27:{
+        name:"cardName",
+        img:"",
+    },
+    28:{
+        name:"cardName",
+        img:"",
+    },
+    29:{
+        name:"cardName",
+        img:"",
+    },
+    30:{
+        name:"cardName",
+        img:"",
+    },
+    31:{
+        name:"cardName",
+        img:"",
+    },
+    32:{
+        name:"cardName",
+        img:"",
+    },
+    33:{
+        name:"cardName",
+        img:"",
+    },
+    34:{
+        name:"Grendel: The wise",
+        img:"images/MonsterImgDB/34.jpg",
+    },
+    35:{
+        name:"cardName",
+        img:"",
+    },
+    36:{
+        name:"Mir",
+        img:"images/MonsterImgDB/36.jpg",
+    },
+    37:{
+        name:"The Legendary Headless Swordman",
+        img:"images/MonsterImgDB/37.jpg",
+    },
+    38:{
+        name:"Hawkeye",
+        img:"images/MonsterImgDB/38.jpg",
+    },
+    39:{
+        name:"cardName",
+        img:"",
+    },
+    40:{
+        name:"Eckhart: The NightWalker",
+        img:"images/MonsterImgDB/20.jpg",
+    },
+    41:{
+        name:"Kinesis",
+        img:"images/MonsterImgDB/41.jpg",
+    },
+    42:{
+        name:"cardName",
+        img:"",
+    },
+    43:{
+        name:"cardName",
+        img:"",
+    },
+    44:{
+        name:"Mr.Penguin",
+        img:"images/MonsterImgDB/44.jpg",
+    },
+    45:{
+        name:"Dual Blade",
+        img:"images/MonsterImgDB/45.jpg",
+    },
+    46:{
+        name:"Chief Bandit",
+        img:"images/MonsterImgDB/46.png",
+    },
+    47:{
+        name:"cardName",
+        img:"",
+    },
+    48:{
+        name:"Cygnus: The Empress of Maple World",
+        img:"images/MonsterImgDB/48.jpg",
+    },
+    49:{
+        name:"Papulatus",
+        img:"images/MonsterImgDB/49.jpg",
+    },
+    50:{
+        name:"Dragon Roar",
+        img:"images/MonsterImgDB/45.png",
+    },
+    51:{
+        name:"King Clang",
+        img:"images/MonsterImgDB/51.png",
+    },
+    52:{
+        name:"Fire Boar",
+        img:"images/MonsterImgDB/52.jpg",
+    },
+    53:{
+        name:"cardName",
+        img:"",
+    },
+    54:{
+        name:"cardName",
+        img:"",
+    },
+    55:{
+        name:"Guard Captain: Darknell",
+        img:"images/MonsterImgDB/55.jpg",
+    },
+    56:{
+        name:"Balrog",
+        img:"images/MonsterImgDB/56.jpg",
+    },
+    57:{
+        name:"cardName",
+        img:"",
+    },
+    58:{
+        name:"cardName",
+        img:"",
+    },
+    59:{
+        name:"Mano",
+        img:"images/MonsterImgDB/59.jpg",
+    },
+}
